fe84a437f67a9397529104d7e5bc9c2be924c676
karanmatta <karanmatta28@gmail.com>
2022-09-30 20:47:49 +0530

    Putting All Work In

---
+++ Day 1/Difference.java
@@ -0,0 +1,14 @@
+public class Difference{
+    public static void main(String[] args){
+        //Variables
+
+        int a= 10;
+        int b = 20;
+        int sum;
+        sum =a + b;
+        int difference =b-a;
+    //System.out.println(sum);
+    System.out.println("DIfference is" +difference);  
+    
+    }
+}
---
+++ Day 1/Multiply.java
@@ -0,0 +1,14 @@
+public class Multiply {
+    
+        public static void main(String[] args){
+            //Variables
+    
+            int a= 10;
+            int b = 20;
+            int multiply =a*b;
+        System.out.println(multiply);
+    
+        }
+    
+    
+}
