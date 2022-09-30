1aa836936eb202551e8c28aa874e7cc69c9ccb28
karanmatta <karanmatta28@gmail.com>
2022-09-30 22:02:29 +0530

    Putting All Work In

---
+++ Day2/Evenodd.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Evenodd {
+    
+
+    public static void main(String[] args){
+        
+        Scanner sc=new Scanner(System.in);
+        int number = sc.nextInt();
+        if(number % 2==0){
+            System.out.println("Even");
+
+        }
+        else{
+            System.out.println("Odd");
+        }
+            
+}
+
+}
---
+++ Day2/Input.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Input {
+    public static void main(String args[]){
+        //Input
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        //Sc.next for single token or word
+        //Sc.nextLine for multiple tokens or word
+        //next Float()
+        //nextInt()
+        System.out.println(name);
+
+    }
+    
+    
+}
