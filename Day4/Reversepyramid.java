d60d502169678298b554ce0fb8473f12d47f7e15
karanmatta <karanmatta28@gmail.com>
2022-10-03 20:01:20 +0530

    initial commit

--- Day4/Reversepyramid.java
+++ Day4/Reversepyramid.java
@@ -2,7 +2,7 @@
  public class Reversepyramid{
    public static void main(String args[]) {
        int n = 4;
-      
+      // decrement operator i=i-1 , i--
        for(int i=n; i>=1; i--) {
            for(int j=1; j<=i; j++) {
                    System.out.print("*");
