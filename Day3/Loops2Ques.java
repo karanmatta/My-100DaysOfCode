9c6b25a978a69995c1691e319374220454a48fac
karanmatta <karanmatta28@gmail.com>
2022-10-06 00:12:45 +0530

    Day 07 Array

--- Day3/Loops2Ques.java
+++
@@ -1,8 +0,0 @@
-public class Loops2Ques {
-    public static void main(String[] args){
-//counter++ =>counter =counter+1
-        for(int i=0; i<11;i++){
-            System.out.println(i);
-        }
-    }
-}
