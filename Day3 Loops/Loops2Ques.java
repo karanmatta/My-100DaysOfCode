5212253f6d737105df70d994546281b2edc37941
karanmatta <karanmatta28@gmail.com>
2022-10-06 13:25:44 +0530

    Putting all work in

---
+++ Day3 Loops/Loops2Ques.java
@@ -0,0 +1,8 @@
+public class Loops2Ques {
+    public static void main(String[] args){
+//counter++ =>counter =counter+1
+        for(int i=0; i<11;i++){
+            System.out.println(i);
+        }
+    }
+}
---
+++ Day3 Loops/Loopwhilecheck.java
@@ -0,0 +1,10 @@
+public class Loopwhilecheck{
+    public static void main(String[] args){
+//counter++ =>counter =counter+1
+int i=12;        
+while(i<11){
+    System.out.println(i);
+    i++;
+}
+    }
+}
