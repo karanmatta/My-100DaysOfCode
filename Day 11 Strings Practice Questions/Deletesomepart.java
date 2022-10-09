389b0fc8555b49f3f6454d6411b3402a103d2aba
karanmatta <karanmatta28@gmail.com>
2022-10-09 20:25:13 +0530

    Putting all work in

---
+++ Day 11 Strings Practice Questions/Deletesomepart.java
@@ -0,0 +1,19 @@
+import java.util.*;
+ 
+public class Deletesomepart {
+   public static void main(String args[]) {
+     Scanner sc = new Scanner (System.in);
+     String email = sc.next();
+     String userName = "";
+ 
+     for(int i=0; i<email.length(); i++) {
+       if(email.charAt(i) == '@') {
+        break;
+       } else {
+         userName += email.charAt(i);
+       }
+     }
+ 
+     System.out.println(userName);
+   }
+}
---
+++ Day 11 Strings Practice Questions/Totallength.java
@@ -0,0 +1,17 @@
+import java.util.*;
+ 
+public class Totallength {
+   public static void main(String args[]) {
+     Scanner sc = new Scanner (System.in);
+     int size = sc.nextInt();
+     String array[] = new String[size];
+     int totLength = 0;
+ 
+     for(int i=0; i<size; i++) {
+       array[i] = sc.next();
+       totLength += array[i].length();
+     }
+ 
+     System.out.println(totLength);
+   }
+}
