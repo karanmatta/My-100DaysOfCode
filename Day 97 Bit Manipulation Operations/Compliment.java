0e7e359dc3f7c16145ee343219667cef907d1d59
karanmatta <karanmatta28@gmail.com>
2023-02-08 21:37:47 +0530

    Day 97 Bit Manipulation Operations- DSA

---
+++ Day 97 Bit Manipulation Operations/Compliment.java
@@ -0,0 +1,10 @@
+public class BitwiseComplimentExample  
+{   
+public static void main(String[] args)   
+{   
+int x = 2;   
+// bitwise compliment  
+// ~0010= 1101 = -3  
+System.out.println("~x = " + (~x));   
+}  
+}  
---
+++ Day 97 Bit Manipulation Operations/Inclusive_Or.java
@@ -0,0 +1,10 @@
+public class BitwiseInclusiveOrExample  
+{   
+public static void main(String[] args)   
+{   
+int x = 9, y = 8;   
+// bitwise inclusive OR  
+// 1001 | 1000 = 1001 = 9  
+System.out.println("x | y = " + (x | y));   
+}  
+}  
